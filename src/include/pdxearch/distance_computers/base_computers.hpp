0b1af6ef6b135467f4e401d1879d713068d8f6e7
Noorts <50550612+Noorts@users.noreply.github.com>
2026-01-28 20:04:54 +0100

    fix: fallback to `ScalarComputer` if SIMD is not supported

--- src/include/pdxearch/distance_computers/base_computers.hpp
+++ src/include/pdxearch/distance_computers/base_computers.hpp
@@ -16,6 +16,11 @@
 #include "pdxearch/distance_computers/avx512_computers.hpp"
 #endif
 
+// Fallback to scalar computer.
+#if !defined(__ARM_NEON) && !defined(__AVX2__) && !defined(__AVX512F__)
+#include "pdxearch/distance_computers/scalar_computers.hpp"
+#endif
+
 // TODO: Support SVE
 
 namespace PDX {
@@ -25,7 +30,11 @@ class DistanceComputer {};
 
 template <>
 class DistanceComputer<L2, Quantization::F32> {
+#if !defined(__ARM_NEON) && !defined(__AVX2__) && !defined(__AVX512F__)
+	using computer = ScalarComputer<L2, F32>;
+#else
 	using computer = SIMDComputer<L2, F32>;
+#endif
 
 public:
 	constexpr static auto VerticalReorderedPruning = computer::VerticalPruning<true, true>;
