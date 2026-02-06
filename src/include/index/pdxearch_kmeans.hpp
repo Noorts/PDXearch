998fb78221521f4e7566c66b67bdf4608e7f8ddd
Noorts <50550612+Noorts@users.noreply.github.com>
2026-02-06 20:10:38 +0100

    feat: add SuperKMeans

--- src/include/index/pdxearch_kmeans.hpp
+++ src/include/index/pdxearch_kmeans.hpp
@@ -2,9 +2,8 @@
 
 #include "duckdb/common/assert.hpp"
 #include <vector>
-#include "faiss/IndexIVFFlat.h"
-#include "faiss/IndexFlat.h"
 #include "pdxearch/common.hpp"
+#include "superkmeans/superkmeans.h"
 
 namespace duckdb {
 
@@ -27,7 +26,7 @@ struct KMeansResult {
 	}
 };
 
-// Compute centroids (clusters) and centroid-to-embedding assignments using FAISS.
+// Compute centroids (clusters) and centroid-to-embedding assignments using SuperKMeans.
 [[nodiscard]] inline KMeansResult ComputeKMeans(const float *const embeddings, const uint64_t num_embeddings,
                                                 const uint32_t num_dimensions, const uint32_t num_clusters,
                                                 const PDX::DistanceMetric distance_metric) {
@@ -35,45 +34,26 @@ struct KMeansResult {
 	D_ASSERT(num_dimensions >= 1);
 	D_ASSERT(num_clusters >= 1);
 
-	faiss::IndexFlatL2 faiss_quantizer(num_dimensions);
-	faiss::IndexIVFFlat faiss_index(&faiss_quantizer, num_dimensions, num_clusters);
-
-	// Configure optional sampling
-	constexpr double SAMPLING_RATIO = 1.0;
-	static_assert(0.0 < SAMPLING_RATIO && SAMPLING_RATIO <= 1.0, "SAMPLING_RATIO must be in (0.0, 1.0].");
-	if constexpr (SAMPLING_RATIO < 1.0) {
-		const int max_embeddings_per_centroid =
-		    static_cast<int>(std::ceil((SAMPLING_RATIO * static_cast<double>(num_embeddings)) / num_clusters));
-		faiss_index.cp.max_points_per_centroid = max_embeddings_per_centroid;
-	}
-
-	faiss_index.cp.spherical =
-	    distance_metric == PDX::DistanceMetric::COSINE || distance_metric == PDX::DistanceMetric::IP;
-
-	// Compute centroids and assignments
-	D_ASSERT(!faiss_index.is_trained);
-	faiss_index.train(static_cast<int64_t>(num_embeddings), embeddings);
-	faiss_index.add(static_cast<int64_t>(num_embeddings), embeddings);
-
 	auto result = KMeansResult(num_dimensions, num_clusters);
 
+	// Compute centroids
+	skmeans::SuperKMeansConfig config;
+	config.sampling_fraction = 1.0f;
+	config.angular = distance_metric == PDX::DistanceMetric::COSINE || distance_metric == PDX::DistanceMetric::IP;
+	config.data_already_rotated = true;
+	auto kmeans = skmeans::SuperKMeans(num_clusters, num_dimensions, config);
+	std::vector<float> centroids = kmeans.Train(embeddings, num_embeddings);
+
 	// Extract centroids
-	faiss_quantizer.reconstruct_n(0, faiss_quantizer.ntotal, result.centroids.get());
+	std::memcpy(result.centroids.get(), centroids.data(), centroids.size() * sizeof(float));
 
 	// Extract assignments
-	for (uint64_t cluster_idx = 0; cluster_idx < num_clusters; cluster_idx++) {
-		const size_t cluster_size = faiss_index.invlists->list_size(cluster_idx);
-		result.assignments[cluster_idx].reserve(cluster_size);
-
-		const faiss::idx_t *faiss_ids = faiss_index.invlists->get_ids(cluster_idx);
-
-		for (uint64_t position_in_cluster = 0; position_in_cluster < cluster_size; position_in_cluster++) {
-			// embedding_idx indexes into the vectors and row_ids arrays. It is not necessarily equal to the row id.
-			auto embedding_idx = static_cast<uint64_t>(faiss_ids[position_in_cluster]);
-			result.assignments[cluster_idx].emplace_back(embedding_idx);
-		}
-
-		faiss_index.invlists->release_ids(cluster_idx, faiss_ids);
+	// SuperKMeans returns assignment from vec_id (not row_id) to centroid_idx
+	std::vector<uint32_t> assignments = kmeans.Assign(embeddings, centroids.data(), num_embeddings, num_clusters);
+	// Convert into assignment from centroid_idx to vec_id (not row_id)
+	result.assignments.resize(num_clusters);
+	for (uint64_t vec_id = 0; vec_id < num_embeddings; vec_id++) {
+		result.assignments[assignments[vec_id]].emplace_back(vec_id);
 	}
 
 	return result;
