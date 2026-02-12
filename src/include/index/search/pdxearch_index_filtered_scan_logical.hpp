aef730d906aeff74a619c49afd8481925f60c470
Noorts <50550612+Noorts@users.noreply.github.com>
2026-02-12 19:02:58 +0100

    refactor: address `cppcoreguidelines-rvalue-reference-param-not-moved` clang-tidy warning

--- src/include/index/search/pdxearch_index_filtered_scan_logical.hpp
+++ src/include/index/search/pdxearch_index_filtered_scan_logical.hpp
@@ -10,7 +10,7 @@ class DuckTableEntry;
 class LogicalPDXearchIndexFilteredScan : public LogicalExtensionOperator {
 public:
 	LogicalPDXearchIndexFilteredScan(DuckTableEntry &table, Index &index, idx_t limit,
-	                                 unsafe_unique_array<float> &&query_vector, vector<ColumnIndex> column_ids,
+	                                 unsafe_unique_array<float> query_vector, vector<ColumnIndex> column_ids,
 	                                 idx_t table_index)
 	    : LogicalExtensionOperator(), column_ids(std::move(column_ids)), table_index(table_index), table(table),
 	      index(index), limit(limit), query_embedding(std::move(query_vector)) {
